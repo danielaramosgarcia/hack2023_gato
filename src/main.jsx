fa9e34a3bce4ad048178ed4127b69c4a7e2c90ed
Erick Siller <102171276+Erick2929@users.noreply.github.com>
2023-09-23 17:47:38 -0600

    router added
    este es el router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LessonView from "./pages/lessonView/index.jsx";
+import MainView from "./pages/mainView/index.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainView />,
+  },
+  {
+    path: "/oladani",
+    element: <LessonView />,
+  },
+  {
+    path: "/main",
+    element: <App />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
