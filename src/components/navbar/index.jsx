e262cd2d679450dca495239f43c6d32a9783a856
Erick Siller <102171276+Erick2929@users.noreply.github.com>
2023-09-23 20:32:04 -0600

    navbar ready

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./styles.module.css";
+import gatito from "../../assets/gatito.png";
+
+function Navbar() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.row}>
+        <img src={gatito} alt='logo' className={styles.image} />
+        <h1>Pluscientia</h1>
+      </div>
+      <div className={styles.row}>
+        <h3>23/09/23</h3>
+        <div className={styles.avatar}>
+          <h1>D</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
