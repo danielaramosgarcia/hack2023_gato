3379392493e78f66eaa43bb0f2fae14b07afe7ae
Erick Siller <102171276+Erick2929@users.noreply.github.com>
2023-09-23 19:25:57 -0600

    link test

--- src/pages/mainView/index.jsx
+++ src/pages/mainView/index.jsx
@@ -1,6 +1,11 @@
+import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
 function MainView() {
-  return <div className={styles.container}>MainView</div>;
+  return (
+    <div className={styles.container}>
+      <Link to={"/login"}>Hla</Link>
+    </div>
+  );
 }
 
 export default MainView;
