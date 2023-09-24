f00614eb2bbb805faf6f49837abbde3589b11430
Daniela Ramos <daniramos5566@hotmail.com>
2023-09-24 11:10:15 -0600

    congratsFinal

--- src/components/modal/Archivo/index.jsx
+++ src/components/modal/Archivo/index.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import styles from "./styles.module.css";
 import { BiDownload } from "react-icons/bi";
+import { useState } from "react";
+
 function Archivo() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedFile(file);
+  };
+
   return (
     <div className={styles.container}>
+          <input
+          type="file"
+          accept=".pdf,.doc,.docx"
+          onChange={handleFileChange}
+          className={styles.fileInput}
+        />
       <BiDownload
         style={{ height: "130px", width: "auto", color: "#3A3A3A" }}
       />
