a61a5d336b7421754a540f1c3a3afccc6447af6d
Erick Siller <102171276+Erick2929@users.noreply.github.com>
2023-09-24 03:17:32 -0600

    Cambios

--- src/pages/mainView/components/MisBases/components/MyBasesModule/components/knowledgeBase/index.jsx
+++ src/pages/mainView/components/MisBases/components/MyBasesModule/components/knowledgeBase/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import styles from "./styles.module.css";
+import { Link } from "react-router-dom";
 function KnowledgeBase({ title, description, picture }) {
   return (
-    <div className={styles.container}>
+    <Link to={"/lesson"} className={styles.container}>
       <div className={styles.upperhalf} style={{ backgroundColor: picture }} />
       <div className={styles.divider}>
         <div className={styles.avatar}>{title[0]}</div>
@@ -11,7 +12,7 @@ function KnowledgeBase({ title, description, picture }) {
         <h3>{title}</h3>
         <p>{description}</p>
       </div>
-    </div>
+    </Link>
   );
 }
 
--- src/pages/mainView/components/MisBases/components/ProgressModule/index.jsx
+++ src/pages/mainView/components/MisBases/components/ProgressModule/index.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import styles from "./styles.module.css";
 import ProgressBar from "../../../../../../components/ProgressBar";
+import { Link } from "react-router-dom";
 function ProgressModule({ image, title, progress }) {
   return (
-    <div className={styles.container}>
-      <div className={styles.iconTitle}>
-        <img src={image} alt='image' />
-        <h6>{title}</h6>
+    <Link to={"/lesson"} style={{ textDecoration: "none" }}>
+      <div className={styles.container}>
+        <div className={styles.iconTitle}>
+          <img src={image} alt='image' />
+          <h6>{title}</h6>
+        </div>
+        <div className={styles.barContainer}>
+          <ProgressBar progress={progress} />
+        </div>
       </div>
-      <div className={styles.barContainer}>
-        <ProgressBar progress={progress} />
-      </div>
-    </div>
+    </Link>
   );
 }
 
