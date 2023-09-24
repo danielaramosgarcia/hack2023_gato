52548c7bf495d4383bb21ff6f7dda17982fded68
R0g3r-SH <rogelio84@hotmail.com>
2023-09-24 11:30:35 -0600

    chat

--- src/pages/mainView/components/Descubrir/index.jsx
+++ src/pages/mainView/components/Descubrir/index.jsx
@@ -1,15 +1,54 @@
-import React from "react";
+import React , {useState, useEffect} from "react";
 import styles from "./styles.module.css";
 import { GiAtom } from "react-icons/gi";
 import { PiGraph } from "react-icons/pi";
 import { BiCoinStack } from "react-icons/bi";
 import { IoSend } from "react-icons/io5";
 
+import axios from "axios";
+
 function Descubrir() {
+  const [inputValue, setInputValue] = useState('');
+  const [conversation, setConversation] = useState([]);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+  const handleSendClick = async () => {
+    try {
+      // Add the user's message to the conversation
+      const userMessage = { role: 'user', content: inputValue };
+      const updatedConversation = [...conversation, userMessage];
+      setConversation(updatedConversation);
+  
+      // Send the messages to the GPT API
+      console.log("Loading...");
+      const response = await axios.post('https://gato-h7wa.onrender.com/generate-chat', {
+        messages: updatedConversation
+      }).catch((error) => {
+        console.error('Error fetching response from GPT API:', error);
+      });
+  
+      // Log the response (stringify for better readability)
+      console.log(response);
+      setInputValue('');
+
+      const assistantMessage = { role: 'assistant', content: response.data };
+      const updatedConversationWithResponse = [...updatedConversation, assistantMessage];
+      setConversation(updatedConversationWithResponse);
+
+    } catch (error) {
+      console.error('Error sending request to GPT API:', error);
+    }
+  };
+  
+  
   return (
     <div className={styles.container}>
       <h1>Descubre nuevos retos para tu conocimiento!</h1>
-      <div className={styles.row}>
+
+
+      { conversation.length==0 ? <div className={styles.row}>
         <div className={styles.moduloSugerencia}>
           <GiAtom className={styles.icon} />
           <div className={styles.text}>
@@ -28,10 +67,33 @@ function Descubrir() {
             Ayudame a saber mas de como cuidar de mi dinero
           </div>
         </div>
+      </div> : <div className={styles.row}/>
+      }
+      
+      <div style={{
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+        {conversation.map((message, index) => (
+          <div key={index} className={styles.message}>
+            {message.role === 'user' ? (
+              <div className={styles.userMessage}>{message.content}</div>
+            ) : (
+              <div className={styles.assistantMessage}>{message.content}</div>
+            )}
+          </div>
+        ))}
       </div>
       <div className={styles.inputRow}>
-        <input type='text' />
-        <IoSend className={styles.icon} />
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <IoSend className={styles.icon} onClick={handleSendClick} />
       </div>
     </div>
   );
